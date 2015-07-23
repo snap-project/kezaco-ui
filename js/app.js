7459dbecf21ed0c8f479f3f95b6f6381bb058b73
Benjamin <benjamin.gaude@ac-dijon.fr>
2015-07-23 09:35:59 +0200

    Formulaires

--- js/app.js
+++ js/app.js
@@ -4,4 +4,24 @@ $(document).ready(function(){
     $('body').toggleClass('with-sidebar');
   });
 
+  $('.field-input').focus(function(){
+    $(this).parent().addClass('is-focused has-label');
+  });
+
+  // à la perte du focus
+  $('.field-input').blur(function(){
+    $parent = $(this).parent();
+    if($(this).val() == ''){
+      $parent.removeClass('has-label');
+    }
+    $parent.removeClass('is-focused');
+  });
+
+  // si un champs est rempli on rajoute directement la class has-label
+  $('.field-input').each(function(){
+    if($(this).val() != ''){
+      $(this).parent().addClass('has-label');
+    }
+  });
+
 });
